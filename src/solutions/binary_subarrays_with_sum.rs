68fc166b482929accc313f4d6c5ce98e125fc1a7
mohamadafzal06 <mohamadafzal06@gmail.com>
2024-03-16 10:38:55 +0000

    add test for num_subarrays.

--- src/solutions/binary_subarrays_with_sum.rs
+++ src/solutions/binary_subarrays_with_sum.rs
@@ -22,3 +22,11 @@ pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
     }
     result
 }
+
+#[cfg(test)]
+pub mod tests{
+    #[test]
+    fn test_num_subarrays_with_sum(){
+
+    }
+}
